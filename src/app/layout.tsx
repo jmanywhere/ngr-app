786f6e1ccc3620e040ad01a8cee5c07ebe0eb2b4
MGalvez91 <galvezcifuentes@gmail.com>
2023-08-21 20:39:13 -0600

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-white">
         <Header />
         <Providers>{children}</Providers>
         <Footer />
