8deaa999b3cbd2a9ced730c975c8f836c425b87e
MGalvez91 <galvezcifuentes@gmail.com>
2023-08-21 20:39:19 -0600

    header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import logo from "../../../public/images/Logo.png";
 import Link from "next/link";
+import Web3HeaderBtn from "./W3HeaderBtn";
+import MenuButton from "./MenuButton";
 
 const Header = () => {
   return (
-    <header className="flex">
-      <Image src={logo} alt="NextGen Logo" />
-      <nav>
+    <header className="flex align-middle justify-between bg-white w-full fixed top-0 z-50">
+      <Image src={logo} alt="NextGen Logo" className="w-[150px] h-[50]" />
+      <MenuButton />
+      <nav className="hidden md:block">
         <Link href="/">Home</Link>
         <a href="#FAQ">FAQ</a>
         <a href="#Lottery">Lotery</a>
