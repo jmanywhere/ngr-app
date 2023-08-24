1437d446eeeabc2ba68cb6d7325025fc07a058b3
MGalvez91 <galvezcifuentes@gmail.com>
2023-08-24 16:32:54 -0600

    app page

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -12,6 +12,7 @@ const Page: NextPage = () => {
   const depositAmount = 0; //fines ilustrativos
   const liquidationAmount = 0; //fines ilustrativos
   const untilAmount = 0; //fines ilustrativos
+  const USDTWallet = 0; //fines ilustrativos
 
   return (
     <main className="pt-[65px] bg-hero-pattern-500 md:bg-hero-pattern-800 lg:bg-hero-pattern-1080 flex flex-col items-center">
@@ -19,8 +20,8 @@ const Page: NextPage = () => {
         Investment
       </h1>
       <section className="flex flex-col px-5 w-full">
-        <div className="text-black px-4 rounded-lg border-4 border-black flex flex-col items-center bg-black bg-opacity-40">
-          <h2 className="font-bold text-2xl py-4">Stats</h2>
+        <div className="text-black px-4 py-4 rounded-lg border-4 border-black flex flex-col items-center bg-black bg-opacity-40 mb-4">
+          <h2 className="font-bold text-2xl pb-4">Stats</h2>
           <div className="w-full px-6 pb-4">
             <div className="flex justify-between">
               <p>TCV</p>
@@ -35,7 +36,7 @@ const Page: NextPage = () => {
               <p>{Liquidations}</p>
             </div>
           </div>
-          <div className="w-full px-6 pb-4 border-b-4 border-black mb-4">
+          <div className="w-full px-6 pb-4 border-b-4 border-[#333] mb-4">
             <div className="flex justify-between">
               <p>Condition 1</p>
               <p>{String(Condition1)}</p>
@@ -49,7 +50,7 @@ const Page: NextPage = () => {
               <p>{String(Condition3)}</p>
             </div>
           </div>
-          <div>
+          <div className="pb-5">
             <h3>Your Stats</h3>
             <div className="flex justify-between">
               <p>Positions:</p>
@@ -62,23 +63,41 @@ const Page: NextPage = () => {
               <button className="px-5"> {">"} </button>
             </div>
           </div>
-          <div className="border-black border-2 rounded-lg">
-            <div className="flex">
+          <div className="border-[#333] border-4 rounded-lg w-full py-3 px-6">
+            <div className="flex justify-between pb-1">
               <p>Deposit Amount:</p>
               <p>{depositAmount}</p>
             </div>
-            <div className="flex">
+            <div className="flex justify-between pb-1">
               <p>Liquidation Amount:</p>
               <p>{liquidationAmount}</p>
             </div>
-            <div className="flex">
+            <div className="flex justify-between pb-1">
               <p>Until Amount:</p>
               <p>{untilAmount}</p>
             </div>
           </div>
         </div>
-        <div className="text-black">
-          <h2>Actions</h2>
+        <div className="text-black py-4 py-4 rounded-lg border-4 border-black flex flex-col items-center bg-black bg-opacity-40 mb-4">
+          <h2 className="font-bold text-2xl pb-4">Actions</h2>
+          <div className="py-3 w-full flex flex-col items-center">
+            <div className="relative">
+              <input
+                type="text"
+                className="input input-bordered w-full max-w-xs "
+              ></input>
+              <button className="absolute ">Max</button>
+              <div className="flex justify-between w-full">
+                <label>USDT Wallet</label>
+                <label>{USDTWallet}</label>
+              </div>
+            </div>
+            <button>deposit</button>
+          </div>
+          <div>
+            <button>Early Withdraw</button>
+            <p>DISCLAIMER PENDING!!!</p>
+          </div>
         </div>
       </section>
     </main>
