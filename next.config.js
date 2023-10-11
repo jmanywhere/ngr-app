d83947dc9fdb0530ae18b0f91cf02eae8462c520
Semi Invader <=>
2023-10-11 09:19:53 -0600

    remove worthless errors

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config, context) => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
+    if (config.plugins) {
+      config.plugins.push(
+        new context.webpack.IgnorePlugin({
+          resourceRegExp: /^(lokijs|pino-pretty|encoding|supports-color)$/,
+        }),
+      )
+    }
     return config
   },
 }
