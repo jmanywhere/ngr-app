be0df04d6c8e4bb9f15278369cd49fad72473272
Semi Invader <=>
2023-10-20 02:46:36 -0600

    updated to another test contract

--- src/data/contracts.ts
+++ src/data/contracts.ts
@@ -7,8 +7,8 @@ export const ngrContract = "0x0aC58925A4C668AB30d29fdBEC267A795d9f7891";
 export const TEST_USDT_ADDRESS = "0xb6d07d107ff8e26a21e497bf64c3239101fed3cf";
 export const USDT_ADDRESS = "0x55d398326f99059fF775485246999027B3197955";
 
-export const growNGR = "0x7a8Bdd04562a656D6367ae60D28483CAce6Ae7Ab";
-export const growToken = "0xc675eE1671bC951cb60874adc94e8777555B2947";
+export const growNGR = "0x9A9E7D83A26936b3b84D19a5A9b79017FA35bD34";
+export const growToken = "0x7fd80138396941B934e24892DF3ba107C3982651";
 
 export const ngrConfig = {
   address: ngrContract,
