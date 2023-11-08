18fdd2416f1fc742413c2640dd003e04a240afba
Semi Invader <=>
2023-11-08 09:29:18 -0600

    get deposit from server, revalidate, every hour

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,8 +1,41 @@
 import { StatsCard } from "@/components/app/Cards";
+import { fi } from "date-fns/locale";
 import { NextPage } from "next";
 import Link from "next/link";
+import { formatEther } from "viem";
 
-const Page: NextPage = () => {
+if (!process.env.BITQUERY_API)
+  throw new Error("BITQUERY_API not set in environment variables");
+
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("X-API-KEY", process.env.BITQUERY_API);
+
+const raw = JSON.stringify({
+  query:
+    'query MyQuery {\n  EVM(network: bsc, dataset: combined) {\n    Events(\n      where: {Log: {SmartContract: {is: "0xCFaFe72b956b19D044722395B2564f8997941Af3"}, Signature: {Name: {is: "Deposit"}}}}\n    ) {\n      Arguments {\n        Name\n        Index\n        Value {\n          ... on EVM_ABI_BigInt_Value_Arg {\n            bigInteger\n          }\n        }\n      }\n    }\n  }\n}\n',
+  variables: "{}",
+});
+
+async function fetchRawGraphQl() {
+  const res = await fetch("https://streaming.bitquery.io/graphql", {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    next: { revalidate: 3600 },
+  });
+
+  const fullData = await res.json();
+  const data = fullData.data.EVM.Events;
+  const totalDeposits = data.reduce((acc: bigint, curr: any) => {
+    const deposit = BigInt(curr.Arguments[2].Value.bigInteger);
+    return acc + deposit;
+  }, 0n);
+  return totalDeposits.toString();
+}
+
+const Page: NextPage = async () => {
+  const depositData = await fetchRawGraphQl();
   return (
     <main className="pt-[65px] bg-hero-pattern-500 md:bg-hero-pattern-800 lg:bg-hero-pattern-1080 flex flex-col items-center">
       <h1 className="text-black text-4xl font-bold pt-5 md:pt-10 uppercase pb-4">
@@ -10,7 +43,11 @@ const Page: NextPage = () => {
       </h1>
 
       <section className="flex flex-col px-5 w-full items-center lg:px-10 pb-5 gap-4">
-        <StatsCard />
+        <StatsCard>
+          {parseFloat(formatEther(depositData)).toLocaleString(undefined, {
+            maximumFractionDigits: 0,
+          })}
+        </StatsCard>
       </section>
     </main>
   );
