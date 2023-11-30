69cf1a4602815d9b3fe5a7dfa72fefbd50ee1e5e
Semi Invader <=>
2023-11-30 00:26:55 -0600

    added global drip stats

--- src/app/invest/page.tsx
+++ src/app/invest/page.tsx
@@ -1,5 +1,16 @@
+import DripStats from "@/components/drip/Stats";
+
 export default function InvestPage() {
   return (
-    <div className="flex flex-col items-center py-10">Stats Coming Soon</div>
+    <div className="flex flex-col items-center py-10 gap-10">
+      <div>
+        <h2 className="pb-4 text-center">
+          <span className="text-secondary text-3xl font-bold  uppercase">
+            Drip
+          </span>
+        </h2>
+        <DripStats />
+      </div>
+    </div>
   );
 }
