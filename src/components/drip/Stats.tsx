84c9f2b7a6379b95fe3b097224f64045344c0841
Semi Invader <=>
2023-11-30 00:27:50 -0600

    show quit value and max claimable

--- src/components/drip/Stats.tsx
+++ src/components/drip/Stats.tsx
@@ -77,37 +77,74 @@ export function DripUserStats() {
     //@ts-ignore
     growAmount: userInfo?.[0].result?.[1] as bigint | undefined,
   };
+
+  console.log({ userInfo });
+  const maxUSDTClaimable = ((deposits || 0n) * 5n) / 1000n;
+  const growUsedPerMaxClaim =
+    ((deposits || 0n) * 5n * parseEther("1") * 100n) /
+    (1000n * 95n * (price || 1n));
   return (
-    <div className="stats stats-vertical md:stats-horizontal shadow text-accent bg-emerald-900">
-      <div className="stat">
-        <p className="stat-title text-slate-300">Deposits</p>
-        <p className="stat-value">{formatTokens(deposits)}</p>
-        <p className="stat-desc">USDT</p>
+    <>
+      <div className="stats stats-vertical md:stats-horizontal shadow text-primary ">
+        <div className="stat">
+          <p className="stat-title text-slate-300">Grow Price</p>
+          <p className="stat-value">{formatTokens(price, 6)}</p>
+          <p className="stat-desc">USDT</p>
+        </div>
+        <div className="stat">
+          <p className="stat-title text-slate-300">User Grow</p>
+          <p className="stat-value text-blue-400">
+            {formatTokens(growAmount, 2)}
+          </p>
+          <p className="stat-desc">GROW</p>
+        </div>
       </div>
-      <div className="stat">
-        <p className="stat-title text-slate-300">Claimed</p>
-        <p className="stat-value">{formatTokens(claimed, 6)}</p>
-        <p className="stat-desc">USDT</p>
+      <div className="stats stats-vertical md:stats-horizontal shadow text-accent bg-emerald-900">
+        <div className="stat">
+          <p className="stat-title text-slate-300">Deposits</p>
+          <p className="stat-value">{formatTokens(deposits)}</p>
+          <p className="stat-desc">USDT</p>
+        </div>
+        <div className="stat">
+          <p className="stat-title text-slate-300">Claimed</p>
+          <p className="stat-value">{formatTokens(claimed, 6)}</p>
+          <p className="stat-desc">USDT</p>
+        </div>
+        <div className="stat">
+          <p className="stat-title text-slate-300">Liq. Rewards</p>
+          <p className="stat-value">{formatTokens(liquidatorEarnings)}</p>
+          <p className="stat-desc">USDT</p>
+        </div>
       </div>
-      <div className="stat">
-        <p className="stat-title text-slate-300">Liq. Rewards</p>
-        <p className="stat-value">{formatTokens(liquidatorEarnings)}</p>
-        <p className="stat-desc">USDT</p>
+      <div className="stats stats-vertical md:stats-horizontal shadow text-accent bg-fuchsia-900">
+        <div className="stat">
+          <p className="stat-title text-slate-300">Quit Value</p>
+          <p className="stat-value text-rose-400">
+            {formatTokens(
+              ((growAmount || 0n) * (price || 0n) * 85n) / parseEther("100"),
+              2
+            )}
+          </p>
+          <p className="stat-desc text-slate-300">USDT</p>
+        </div>
+        <div className="stat">
+          <p className="stat-title text-slate-300">Daily Claim</p>
+          <p className="stat-value">
+            {formatTokens(((deposits || 0n) * 5n) / 1000n, 2)}
+          </p>
+          <p className="stat-desc">USDT</p>
+        </div>
+        <div className="stat">
+          <p className="stat-title text-slate-300">Max Claimable</p>
+          <p className="stat-value">
+            {formatTokens(
+              ((growAmount || 0n) * maxUSDTClaimable) / growUsedPerMaxClaim,
+              10
+            )}
+          </p>
+          <p className="stat-desc">USDT</p>
+        </div>
       </div>
-      <div className="stat">
-        <p className="stat-title text-slate-300">Daily Claim</p>
-        <p className="stat-value">
-          {formatTokens(((deposits || 0n) * 5n) / 1000n, 2)}
-        </p>
-        <p className="stat-desc">USDT</p>
-      </div>
-      <div className="stat">
-        <p className="stat-title text-slate-300">Total Claimable</p>
-        <p className="stat-value">
-          {formatTokens(((growAmount || 0n) * (price || 0n)) / parseEther("1"))}
-        </p>
-        <p className="stat-desc">USDT</p>
-      </div>
-    </div>
+    </>
   );
 }
