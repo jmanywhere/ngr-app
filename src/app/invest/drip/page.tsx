9dafa1e9878934546eb6ff56c36a2928701dacbc
Semi Invader <=>
2023-11-30 00:46:48 -0600

    added link to drip liquidator page

--- src/app/invest/drip/page.tsx
+++ src/app/invest/drip/page.tsx
@@ -2,13 +2,20 @@ import DripActions from "@/components/drip/Actions";
 import { DripUserStats } from "@/components/drip/Stats";
 import Image from "next/image";
 import NGRDripLogo from "@/../public/ngrdrip.png";
+import Link from "next/link";
 
 export default function DripPage() {
   return (
     <main className="flex flex-col items-center">
-      <h1 className="text-secondary text-3xl font-bold  uppercase pb-4 ">
+      <h1 className="text-secondary text-3xl font-bold  uppercase pt-4">
         Your Drip
       </h1>
+      <Link
+        href="/invest/drip/liquidator"
+        className="btn btn-link mb-2 text-secondary"
+      >
+        <span>Liquidator view &gt;&gt;</span>
+      </Link>
       <section className="flex flex-col px-1 sm:px-4 w-full items-center lg:px-10 pb-5 gap-4">
         <DripUserStats />
       </section>
